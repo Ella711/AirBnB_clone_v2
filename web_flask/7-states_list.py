342fe82b66a5940f7fad03a9cd2a05fbcf04535b
Ella711 <allenro711@gmail.com>
2022-05-24 15:45:14 -0500

    [FIX] - Files for task8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application:
-   - Routes:
-      - /states_list: display an HTML page: (inside the tag BODY)
-      - H1 tag: "States"
-          - UL tag: with the list of all State objects present in
-              DBStorage sorted by name (A->Z) tip
-          - LI tag: description of one State: <state.id>:
-              <B><state.name></B>
+Script that starts a Flask web application
 """
 from flask import Flask, render_template
 from models import storage
@@ -18,17 +11,17 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """ Remove the current SQLAlchemy Session """
-    storage.close()
-
-
 @app.route('/states_list')
 def fetch_states():
     """ Fetch all states and display on html page """
     states = storage.all(State)
     return render_template('7-states_list.html', state_objs=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
 
 
 if __name__ == "__main__":
