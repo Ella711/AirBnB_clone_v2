d0f662d8a626f0bafab5fb3c0ccfdfbe3ca12349
Ella711 <allenro711@gmail.com>
2022-05-24 16:03:41 -0500

    [FIX] - Files for task9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-# Script that starts a Flask web application:
-#   - Your web application must be listening on 0.0.0.0, port 5000
-#   - /cities_by_states: display an HTML page: (inside the tag BODY)
-#       - H1 tag: "States"
-#       - UL tag: with the list of all State objects present in DBStorage
-#           sorted by name (A->Z) tip
-#           - LI tag: description of one State: <state.id>:
-#               <B><state.name></B>
-#               + UL tag: with the list of City objects linked to the
-#                   State sorted by name (A->Z)
-#               - LI tag: description of one City: <city.id>:
-#                   <B><city.name></B>
+"""Script that starts a Flask web application:
+  - Your web application must be listening on 0.0.0.0, port 5000
+  - /cities_by_states: display an HTML page: (inside the tag BODY)
+      - H1 tag: "States"
+      - UL tag: with the list of all State objects present in DBStorage
+          sorted by name (A->Z) tip
+          - LI tag: description of one State: <state.id>:
+              <B><state.name></B>
+              + UL tag: with the list of City objects linked to the
+                  State sorted by name (A->Z)
+              - LI tag: description of one City: <city.id>:
+                  <B><city.name></B>"""
 
 
 from flask import Flask, render_template
