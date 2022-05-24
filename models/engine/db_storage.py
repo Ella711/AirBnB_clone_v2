2cd7109b9693ce97fb9f3529d8043ec88e7810ca
Ella711 <allenro711@gmail.com>
2022-05-24 14:31:48 -0500

    [FIX] - comments

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """This module defines the engine for the DBStorage"""
-from models.base_model import Base
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.base_model import Base
+from models.city import City
+from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
 from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
